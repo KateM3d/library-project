7c89b682aa11eefdcaff4e299e065e8f13f9109c
Marina Moroz <moroz2387moroz@yandex.ru>
2022-12-04 23:54:32 +1000

    Add books first part

--- src/Book.java
+++ src/Book.java
@@ -5,4 +5,18 @@ public class Book {
     int datePublished;
     String dateBorrowed;
     String dateReturned;
+
+    public Book(String name, String author, String genre, int datePublished, String dateBorrowed, String dateReturned) {
+        this.name = name;
+        this.author = author;
+        this.genre = genre;
+        this.datePublished = datePublished;
+        this.dateBorrowed = dateBorrowed;
+        this.dateReturned = dateReturned;
+    }
+
+    public String toString() {
+        return name + " " + author + " " + genre + " " + datePublished + " " + dateBorrowed + " " + dateReturned;
+    }
+
 }
